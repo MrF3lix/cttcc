b33dbe91913f05f316870ecb27905976abd4d9e7
Niklas van der Heide <vandenik@students.zhaw.ch>
2022-06-16 22:22:40 +0200

    toggle DC inputs

--- components/calculator-form.jsx
+++ components/calculator-form.jsx
@@ -1,89 +1,100 @@
-import { InputNumber } from "./form/input-number";
-import { InputSelect } from "./form/input-select";
+import {InputNumber} from "./form/input-number";
+import {InputSelect} from "./form/input-select";
 import ExtensionButton from "./extension-button";
+import {useState} from "react";
 
-export const CalculatorForm = () => (
-    <form className="space-y-8 divide-y divide-gray-200">
-        <div className="space-y-8 divide-y divide-gray-200 sm:space-y-5">
-            <div className="pt-8 space-y-6 sm:pt-10 sm:space-y-5">
-                <div>
-                    <h3 className="text-lg leading-6 font-medium text-gray-900">Default Inputs</h3>
-                    <p className="mt-1 max-w-2xl text-sm text-gray-500">Fill in all the given information and see the magic happen.</p>
+export const CalculatorForm = () => {
+    
+    const [displayDCInputs, setDisplayDCInputs ]= useState(false)
+    const toggleDCInputs = () => setDisplayDCInputs(!displayDCInputs)
+    
+    return (
+        <form className="space-y-8 divide-y divide-gray-200">
+            <div className="space-y-8 divide-y divide-gray-200 sm:space-y-5">
+                <div className="pt-8 space-y-6 sm:pt-10 sm:space-y-5">
+                    <div>
+                        <h3 className="text-lg leading-6 font-medium text-gray-900">Default Inputs</h3>
+                        <p className="mt-1 max-w-2xl text-sm text-gray-500">Fill in all the given information and see
+                            the magic happen.</p>
+                    </div>
+                    <div className="space-y-6 sm:space-y-5">
+                        <InputNumber
+                            label="Pulses [Hz]"
+                            defaultValue={84_000_000}
+                            description="Describes the incoming frequency from either the internal clock or an other timers."
+                        />
+                        <InputNumber
+                            label="Prescaler"
+                            defaultValue={8_400}
+                            description="Scales the incoming pulse by a defined value. Only every n-th pulse is sent to the counter."
+                        />
+                        <InputNumber
+                            label="AAR"
+                            defaultValue={2_000}
+                            description="Auto Reload Register. Defines after how many ticks the counter should reset the initial state."
+                        />
+                        <InputNumber
+                            label="Ticks [Hz]"
+                            defaultValue={10_000}
+                            description="Ticks equal the output for the prescaler (Pulse/Prescaler)."
+                        />
+                        <InputNumber
+                            label="Period [ns]"
+                            defaultValue={100_000}
+                            description="Time it takes for one tick."
+                        />
+                        <InputNumber
+                            label="Time to Overflow [ms"
+                            defaultValue={200}
+                            description="Time it takes for one tick."
+                        />
+                        <InputSelect
+                            label="Mode"
+                            defaultValue={0}
+                            options={[
+                                {value: 0, label: 'Up Counting'},
+                                {value: 1, label: 'Down Counting'}
+                            ]}
+                        />
+                    </div>
                 </div>
-                <div className="space-y-6 sm:space-y-5">
-                    <InputNumber
-                        label="Pulses [Hz]"
-                        defaultValue={84_000_000}
-                        description="Describes the incoming frequency from either the internal clock or an other timers."
-                    />
-                    <InputNumber
-                        label="Prescaler"
-                        defaultValue={8_400}
-                        description="Scales the incoming pulse by a defined value. Only every n-th pulse is sent to the counter."
-                    />
-                    <InputNumber
-                        label="AAR"
-                        defaultValue={2_000}
-                        description="Auto Reload Register. Defines after how many ticks the counter should reset the initial state."
-                    />
-                    <InputNumber
-                        label="Ticks [Hz]"
-                        defaultValue={10_000}
-                        description="Ticks equal the output for the prescaler (Pulse/Prescaler)."
-                    />
-                    <InputNumber
-                        label="Period [ns]"
-                        defaultValue={100_000}
-                        description="Time it takes for one tick."
-                    />
-                    <InputNumber
-                        label="Time to Overflow [ms"
-                        defaultValue={200}
-                        description="Time it takes for one tick."
-                    />
-                    <InputSelect
-                        label="Mode"
-                        defaultValue={0}
-                        options={[
-                            { value: 0, label: 'Up Counting' },
-                            { value: 1, label: 'Down Counting' }
-                        ]}
-                    />
-                </div>
+
+                <ExtensionButton action={toggleDCInputs} />
+
+                {displayDCInputs &&
+                    <div className="pt-8 space-y-6 sm:pt-10 sm:space-y-5">
+                        <div>
+                            <h3 className="text-lg leading-6 font-medium text-gray-900">Duty Cycle Inputs</h3>
+                            <p className="mt-1 max-w-2xl text-sm text-gray-500">Fill in all the given information and see
+                                the magic happen.</p>
+                        </div>
+                        <div className="space-y-6 sm:space-y-5">
+                            <InputNumber
+                                label="Duty Cycle [%]"
+                            />
+                            <InputNumber
+                                label="CCR"
+                            />
+                            <InputSelect
+                                label="PWM Mode"
+                                defaultValue={0}
+                                options={[
+                                    {value: 0, label: 'Mode 1'},
+                                    {value: 1, label: 'Mode 1'}
+                                ]}
+                            />
+                        </div>
+                    </div>
+                }    
             </div>
-            
-            <ExtensionButton/>
-            
-            <div className="pt-8 space-y-6 sm:pt-10 sm:space-y-5">
-                <div>
-                    <h3 className="text-lg leading-6 font-medium text-gray-900">Duty Cycle Inputs</h3>
-                    <p className="mt-1 max-w-2xl text-sm text-gray-500">Fill in all the given information and see the magic happen.</p>
-                </div>
-                <div className="space-y-6 sm:space-y-5">
-                    <InputNumber
-                        label="Duty Cycle [%]"
-                    />
-                    <InputNumber
-                        label="CCR"
-                    />
-                    <InputSelect
-                        label="PWM Mode"
-                        defaultValue={0}
-                        options={[
-                            { value: 0, label: 'Mode 1' },
-                            { value: 1, label: 'Mode 1' }
-                        ]}
-                    />
-                </div>
+            <div className="pt-5">
+                <button
+                    type="submit"
+                    className="justify-center py-2 px-10 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                >
+                    Calculate
+                </button>
             </div>
-        </div>
-        <div className="pt-5">
-            <button
-                type="submit"
-                className="justify-center py-2 px-10 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-            >
-                Calculate
-            </button>
-        </div>
-    </form>
-)
+        </form>
+    )
+}
--- components/extension-button.jsx
+++ components/extension-button.jsx
@@ -1,6 +1,6 @@
 import { PlusSmIcon } from '@heroicons/react/solid'
 
-export default function ExtensionButton() {
+export default function ExtensionButton({ action }) {
     return (
         <div className="pt-8 pb-4">
             <div className="relative">
@@ -11,9 +11,10 @@ export default function ExtensionButton() {
                     <button
                         type="button"
                         className="inline-flex items-center shadow-sm px-4 py-1.5 border border-gray-300 text-sm leading-5 font-medium rounded-full text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                        onClick={action}
                     >
                         <PlusSmIcon className="-ml-1.5 mr-1 h-5 w-5 text-gray-400" aria-hidden="true" />
-                        <span>Add Duty Cycle Inputs</span>
+                        <span>Toggle Duty Cycle Inputs</span>
                     </button>
                 </div>
             </div>
