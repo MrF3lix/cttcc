a86bc4dc0023c60c546a37a88533d296be315b93
Niklas van der Heide <vandenik@students.zhaw.ch>
2022-06-17 22:58:54 +0200

    retouch title

--- pages/index.jsx
+++ pages/index.jsx
@@ -79,12 +79,13 @@ const Index = () => {
 
     return (
         <div className="max-w-7xl mx-auto py-6 px-6 lg:px-8">
-            <div className="text-center mt-24">
+            <div className="text-center mt-16">
                 <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
                     <span className="block xl:inline">Timer/Counter </span>
                     <span className="block text-indigo-600 xl:inline">Calculator</span>
                 </h1>
-                <p className="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">A simple to use calculator for all your CT2 exam questions considering Timers and Counters. By Felix Saaro and Niklas van der Heide</p>
+                <p className="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">A simple calculator for all your CT2 questions considering Timers and Counters.</p>
+                <p className="mt-1 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-1 md:text-xl md:max-w-3xl">By Felix Saaro and Niklas van der Heide</p>
             </div>
             <CalculatorForm onSubmit={onCalculateFormSubmit} error={error} />
             <Result result={result} />
